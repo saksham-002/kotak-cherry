c2358ec9f0b5a41b254faae09d4b7bc21b1fe95b
root <root@ip-10-10-1-127.ap-south-1.compute.internal>
2024-08-30 09:18:40 +0000

    added build files for express-api

---
+++ express_api/docker-build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+#Go to root directory
+BASEDIR=$(dirname "$0")
+
+#Account variables
+AWS_REGION="ap-south-1"
+REGISTRY="861276100864.dkr.ecr.ap-south-1.amazonaws.com"
+
+tag="${epoch}"
+git_branch=${BRANCH_NAME}
+epoch=$(date +%s)
+ecr_repo_name="express-api"
+
+aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 861276100864.dkr.ecr.ap-south-1.amazonaws.com
+
+echo "*** Building Docker image: ${REGISTRY}/${ecr_repo_name}:${tag} ***"
+docker build -t ${REGISTRY}/${ecr_repo_name}:${tag} .
+echo "Image built successfully".
+
+echo "Pushing image to Docker Registry."
+docker push ${REGISTRY}/${ecr_repo_name}:${tag}
+echo "Image pushed successfully."
+
+docker rmi -f ${REGISTRY}/${ecr_repo_name}:${tag}
+echo "Deleted image from build server.
